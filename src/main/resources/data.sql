07d43996060fc144b07d32b23c1f1c0e4e8a0c20
itisvibhor <68295212+itisvibhor@users.noreply.github.com>
2022-07-10 21:52:23 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into user_table (username,password,role) values ('Sufiyan','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Akshay','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Sanchit','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Vishal','1234','ROLE_Admin');
+insert into user_table (username,password,role) values ('Vibhor','1234','ROLE_Admin');
+insert into user_table (username,password,role) values ('Divya','1234','ROLE_Admin');
+insert into user_table (username,password,role) values ('Diksha','1234','ROLE_Admin');
+insert into user_table (username,password,role) values ('Priyanshi','1234','ROLE_Admin');
 
