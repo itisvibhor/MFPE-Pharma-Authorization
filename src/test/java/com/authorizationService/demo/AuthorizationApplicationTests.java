717a32765bfcaa52dbe4ea7e7cfb53e4039ffc29
itisvibhor <68295212+itisvibhor@users.noreply.github.com>
2022-07-10 20:44:04 +0530

    Add files via upload

---
+++ src/test/java/com/authorizationService/demo/AuthorizationApplicationTests.java
@@ -0,0 +1,19 @@
+package com.authorizationService.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+	
+	@Test
+	public void applicationContextTest() {
+	    AuthorizationApplication.main(new String[] {});
+	}
+
+}
