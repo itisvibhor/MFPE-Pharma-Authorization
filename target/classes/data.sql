1dcebefb4beb1b40e97a8d76f64e631dd82547db
itisvibhor <68295212+itisvibhor@users.noreply.github.com>
2022-07-12 16:52:07 +0530

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into user_table (username,password,role) values ('Sufiyan','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Akshay','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Sanchit','1234','ROLE_Admin');
-insert into user_table (username,password,role) values ('Vishal','1234','ROLE_Admin');
-
